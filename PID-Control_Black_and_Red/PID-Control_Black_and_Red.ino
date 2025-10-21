db90a93a86ca2f8ecf520799d409eef372363ce9
kmuroo <kmuroo@gmail.com>
2025-10-21 17:04:32 +0900

    Ti, TdとKi, Kd を混同していたのを修正 Ver. 2.3 → Ver. 2.4
    Ti, TdとKi, Kd を混同していたのを修正 2025/10/20

--- PID-Control_Black_and_Red/PID-Control_Black_and_Red.ino
+++ PID-Control_Black_and_Red/PID-Control_Black_and_Red.ino
@@ -1,4 +1,4 @@
-// Ver. 2.3
+// Ver. 2.4 2025/10/21
 //不明な点があったら芦澤まで
 
 float Target_Black = -18.0; //目標温度
@@ -16,10 +16,14 @@ bool echo_on = false;
 //float Kd_Black = 100.0; //微分ゲイン
 //----- 芦澤、西 における初期設定値 END --------------------
 //----- ジーグラ・ニコルス限界感度法における初期設定値  2025/7/23 変更--------------------
-float Kp_Black = 57.0; //比例ゲイン
-float Ki_Black = 2.18; //積分ゲイン
-//float Kd_Black = 0.544; //微分ゲイン
-float Kd_Black = 1.11; //微分ゲインの微調整 Redと同値へ 20250729
+//----- Ti, TdとKi, Kd を混同していたのを修正 2025/1021 変更---------------------------
+float Ku_Black = 95.0;
+float Pu_Black = 4.35; 
+float Kp_Black = 0.60*Ku_Black;     //比例ゲイン 57.0
+float Ti_Black = 0.50*Pu_Black;     //2.175
+float Td_Black = 0.125*Pu_Black;    //0.54375
+float Ki_Black = Kp_Black/Ti_Black; //積分ゲイン 26.3 
+float Kd_Black = Kp_Black*Td_Black; //微分ゲイン 30.75
 //Black channel
 
 //-30℃で1200/50/0
@@ -30,9 +34,14 @@ float Kd_Black = 1.11; //微分ゲインの微調整 Redと同値へ 20250729
 //float Kd_Red = 33.0; //微分ゲイン
 //----- 芦澤、西 における初期設定値 END --------------------
 //----- ジーグラ・ニコルス限界感度法における初期設定値  2025/7/23 変更--------------------
-float Kp_Red = 72.0; //比例ゲイン
-float Ki_Red = 4.43; //積分ゲイン
-float Kd_Red = 1.11; //微分ゲイン
+//----- Ti, TdとKi, Kd を混同していたのを修正 2025/1021 変更---------------------------
+float Ku_Red = 120.0;
+float Pu_Red = 1.11; 
+float Kp_Red = 0.60*Ku_Red;     //比例ゲイン 72
+float Ti_Red = 0.50*Pu_Red;     //0.555
+float Td_Red = 0.125*Pu_Red;    //0.139
+float Ki_Red = Kp_Red/Ti_Red; //積分ゲイン 129.7 
+float Kd_Red = Kp_Red*Td_Red; //微分ゲイン 48.0
 //Red channel
 
 float P_Black = 0;
